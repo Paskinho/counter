c9541868c026e1b29487db661b1550690870235e
Paskinho <v.paskanov@gmail.com>
2023-02-04 15:17:18 +0300

    Edit type for AC counter-reducer.ts

--- src/redux/counter-reducer.ts
+++ src/redux/counter-reducer.ts
@@ -23,10 +23,35 @@ export const counterReducer = ()=> {
 
 }
 
-export type ActionsType = incrCurrentActionType ;
+export type ActionsType = incrCurrentValueActionType
+    | resetValueActionType
+    | setEditModeActionType
+    | setMinValueActionType
+    | setMaxValueActionType;
 
-export type incrCurrentActionType = {
+export type incrCurrentValueActionType = {
     type: "INCREMENT-CURRENT-VALUE",
+    maxValue: number,
+    current: number
+}
+export type resetValueActionType = {
+    type: "RESET-VALUE",
+    current: number
+}
+
+export type setEditModeActionType = {
+    type: "SET-EDT-MODE",
+    isEditMode: boolean
+}
+
+export type setMinValueActionType = {
+    type: "SET-MIN-VALUE",
+    startValue: number,
+    current: number
+}
+
+export type setMaxValueActionType = {
+    type: "SET-MAX-VALUE",
     maxValue: number,
     current: number
 }
