d7a2aff740c544d793dc1e8faecc28e656bb17b5
Paskinho <v.paskanov@gmail.com>
2023-02-05 15:47:27 +0300

    Add in index.tsx store

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from ;'./serviceWorker'
-import { createRoot } from 'react-dom/client';
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from "./reportWebVitals";
 
-const container  = document.getElementById('root') as HTMLElement
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
